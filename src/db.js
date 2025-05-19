51be04ee139cf9d4a50eca7ca458a1498a96e012
Jose Antonio Vazquez <jv319069@gmail.com>
2025-05-19 20:41:02 +0200

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql2/promise');
+const mysql = require('mysql2');
 require('dotenv').config();
 
 const pool = mysql.createPool({
@@ -12,4 +12,4 @@ const pool = mysql.createPool({
   queueLimit: 0,
 });
 
-module.exports = pool;
+module.exports = pool.promise();
